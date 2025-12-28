1bb30ccb590d8943c05c58ebe79451aa3970b587
Prateek <106758759+Prateek7071@users.noreply.github.com>
2025-12-28 18:35:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
-function firstWord(s) {
-  // your code here
+function firstWord(str) {
+
+  if (str.length === 0) return str;
+  else {
+    str = str.trim().split(" ");
+    return str[0]
+  }
+
+	
 }
 
 // Do not change the code below
 
-const s = prompt("Enter String:");
+//const s = prompt("Enter String:");
 alert(firstWord(s));
